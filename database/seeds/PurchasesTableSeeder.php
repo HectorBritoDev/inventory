14543fc538a0ddcbcb42b680825074bf1d0a60f2
HectorBritoDev <hectorbritodev@gmail.com>
2020-03-26 15:54:03 -0400

    added seeders and logic

---
+++ database/seeds/PurchasesTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PurchasesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        factory(App\Purchase::class, 10)->create()->each(function ($purchase) {
+            $product = App\Product::inRandomOrder()->first();
+            factory(App\PurchaseItem::class, 10)->create([
+                'purchase_id' => $purchase->id,
+                'name' => $product->name,
+                'code' => $product->code,
+                'price' => $product->unit_price,
+            ]);
+        });
+    }
+}
