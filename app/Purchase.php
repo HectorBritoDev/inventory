67e575694f5a70c6725495f2c10be029199ef225
HectorBritoDev <hectorbritodev@gmail.com>
2020-04-04 20:35:25 -0400

    added items relationship to purchase

--- app/Purchase.php
+++ app/Purchase.php
@@ -7,4 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 class Purchase extends Model
 {
     protected $fillable = ['message', 'total_price'];
+
+    public function items()
+    {
+        return $this->hasMany('App\PurchaseItem');
+    }
+
+    // public function products()
+    // {
+    //     return $this->hasManyThrough(
+    //         'App\Product', //Target model
+    //         'App\PurchaseItem', //Through model
+    //         'product_id', // wich field should i look on Through model?
+    //         'id', // wich field should i look on Target model?
+    //         'id', // local id on Though model
+    //         'id' //local key on Target model
+
+    //     );
+    // }
 }
