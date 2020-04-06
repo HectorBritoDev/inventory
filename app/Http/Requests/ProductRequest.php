dfa79df50834c947dcb6301bd4451f6ec4f3cff5
HectorBritoDev <hectorbritodev@gmail.com>
2020-04-06 16:44:56 -0400

    refactore Requests classes in only 1 Request file per model

---
+++ app/Http/Requests/ProductRequest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class ProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        switch ($this->method()) {
+            case 'POST':
+                return [
+                    'category_id' => ['nullable', 'exists:categories,id'],
+                    'code' => ['nullable', 'unique:products,code'],
+                    'name' => ['required', 'string'],
+                    'quantity' => ['required', 'float'],
+                    'mayor_price' => ['nullable', 'float'],
+                    'unit_price' => ['nullable', 'float'],
+                    'minimum_to_apply_mayoritary_price' => ['nullable', 'integer'],
+                ];
+                break;
+
+            case ('PUT' || 'PATCH'):
+                return [
+                    'category_id' => ['nullable', 'exists:categories,id'],
+                    'name' => ['required', 'string'],
+                    'unit_price' => ['nullable', 'integer'],
+                    'mayor_price' => ['nullable', 'integer'],
+                    'minimum_to_apply_mayoritary_price' => ['nullable', 'integer'],
+                    'code' => [
+                        'nullable',
+                        'string',
+                        Rule::unique('products')->ignore($this->product->id),
+                    ],
+                ];
+
+            default:
+                return [];
+                break;
+        }
+    }
+}
