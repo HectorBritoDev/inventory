8f06dee07b74a1730117e3b61cce90f624de51a8
HectorBritoDev <hectorbritodev@gmail.com>
2020-04-06 20:13:42 -0400

    added transaction logic to store

--- app/Http/Controllers/Purchase/PurchaseController.php
+++ app/Http/Controllers/Purchase/PurchaseController.php
@@ -2,16 +2,16 @@
 
 namespace App\Http\Controllers\Purchase;
 
+use App\Http\Controllers\ApiController;
+use App\Http\Requests\StorePurchase;
+use App\Http\Resources\Purchase as PurchaseResource;
+use App\Http\Resources\PurchaseCollection;
 use App\Product;
 use App\Purchase;
 use App\PurchaseItem;
 use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use App\Http\Requests\StorePurchase;
-use App\Http\Controllers\ApiController;
-use App\Http\Resources\PurchaseCollection;
-use App\Http\Resources\Purchase as PurchaseResource;
 
 class PurchaseController extends ApiController
 {
@@ -24,28 +24,27 @@ class PurchaseController extends ApiController
 
     public function store(StorePurchase $request)
     {
-        try{
+        try {
             DB::beginTransaction();
-                $purchase = Purchase::create([]);
-                $items = [];
-                foreach ($request->items as $item) {
-                    $product = Product::findOrFail($item['product_id']);
-                    $product->increment('quantity', $item['quantity']);
-                    $items[] = [
-                        'purchase_id' => $purchase->id,
-                        'name' => $product->name,
-                        'price' => ($product->minimum_to_mayoritary_price >= $item['quantity']) ? $product->mayor_price : $product->unit_price,
-                        'discount' => $item['discount'],
-                        'quantity' => $item['quantity'],
-                    ];
-                }
-                PurchaseItem::insert($items);
-                DB::commit();
-            } catch  (\Throwable $th){
-                DB::rollback();
-                return $this->errorResponse($th->getMessage(),500);
+            $purchase = Purchase::create([]);
+            $items = [];
+            foreach ($request->items as $item) {
+                $product = Product::findOrFail($item['product_id']);
+                $product->increment('quantity', $item['quantity']);
+                $items[] = [
+                    'purchase_id' => $purchase->id,
+                    'name' => $product->name,
+                    'price' => ($product->minimum_to_mayoritary_price >= $item['quantity']) ? $product->mayor_price : $product->unit_price,
+                    'quantity' => $item['quantity'],
+                ];
             }
-            return new PurchaseResource($purchase->load('items'));
+            PurchaseItem::insert($items);
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollback();
+            return $this->errorResponse($th->getMessage(), 500);
+        }
+        return new PurchaseResource($purchase->load('items'));
     }
 
     public function show(Purchase $purchase)
