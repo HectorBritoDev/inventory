78c3a5bd78e7ab44fa26dc1ca95d8adac2a4d2cb
HectorBritoDev <hectorbritodev@gmail.com>
2020-04-22 22:07:23 -0400

    added PurchaseStadisticController

---
+++ app/Http/Controllers/Stadistic/PurchaseStadisticController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Stadistic;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PurchaseStadisticController extends Controller
+{
+    //
+}
