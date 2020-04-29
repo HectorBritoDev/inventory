893b53a185770668202a543a0c065d41147f3f74
HectorBritoDev <hectorbritodev@gmail.com>
2020-04-29 17:50:42 -0400

    added Id and stadistics to Product Resource

--- app/Http/Resources/Product.php
+++ app/Http/Resources/Product.php
@@ -3,7 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
-use App\SaleItem;
+
 class Product extends JsonResource
 {
     /**
@@ -14,17 +14,21 @@ class Product extends JsonResource
      */
     public function toArray($request)
     {
-        // dd($this->sales->first()->quantity);
+        $category = $this->category_id != null;
         return [
-            'id'=> $this->id,
+            'id' => $this->id,
             'name' => $this->name,
-            'avaliable' => $this->quantity,
+            'available' => $this->quantity,
             'unitary_price' => $this->unit_price,
             'mayoritary_price' => $this->mayor_price,
             'purchased_price' => $this->purchase_price,
-            'category_url' => $this->category_id ? route('categories.show', $this->category_id) : null,
-            'times_sold'=>  $this->sales->count()>0 ? $this->sales->first()->quantity_units : 0 
-            ];
+            'category_url' => $this->when($category, function () {
+                return route('categories.show', $this->category_id);
+            }),
+            'stadistics' => [
+                'total_units_sold' => $this->sales->count() > 0 ? $this->sales->first()->total_units_sold : 0,
+            ],
+        ];
         // return parent::toArray($request);
     }
 }
