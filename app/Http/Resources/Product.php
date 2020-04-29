6afa9be043aef68941ff176b01af0f45f750017e
HectorBritoDev <hectorbritodev@gmail.com>
2020-04-28 21:35:15 -0400

    changed the product response

--- app/Http/Resources/Product.php
+++ app/Http/Resources/Product.php
@@ -3,7 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
-
+use App\SaleItem;
 class Product extends JsonResource
 {
     /**
@@ -14,14 +14,17 @@ class Product extends JsonResource
      */
     public function toArray($request)
     {
+        // dd($this->sales->first()->quantity);
         return [
+            'id'=> $this->id,
             'name' => $this->name,
             'avaliable' => $this->quantity,
             'unitary_price' => $this->unit_price,
             'mayoritary_price' => $this->mayor_price,
             'purchased_price' => $this->purchase_price,
-            'category_url' => route('categories.show', $this->category_id),
-        ];
-        return parent::toArray($request);
+            'category_url' => $this->category_id ? route('categories.show', $this->category_id) : null,
+            'times_sold'=>  $this->sales->count()>0 ? $this->sales->first()->quantity_units : 0 
+            ];
+        // return parent::toArray($request);
     }
 }
