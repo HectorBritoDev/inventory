574df416ea37f12f6beb6e6a05e498d43007d1c5
HectorBritoDev <hectorbritodev@gmail.com>
2020-04-28 21:36:00 -0400

    product factory now gives a .now() name to avoid repetitions

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,8 +7,8 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        'name' => $faker->word,
-        'code' => $faker->ean8,
+        'name' => $faker->word . now(),
+        // 'code' => $faker->ean8 . now(),
         'quantity' => $faker->randomNumber(3),
         'unit_price' => $faker->randomNumber(7),
         'mayor_price' => $faker->randomNumber(6),
