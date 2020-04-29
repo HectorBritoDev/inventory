0bd230dfa2c3a1324e22ab4ab180a4b8827ed46f
HectorBritoDev <hectorbritodev@gmail.com>
2020-04-29 19:09:19 -0400

    added purchase relationship

--- app/Product.php
+++ app/Product.php
@@ -24,4 +24,8 @@ class Product extends Model
     {
         return $this->hasMany('App\SaleItem');
     }
+    public function purchases()
+    {
+        return $this->hasMany('App\PurchaseItem');
+    }
 }
